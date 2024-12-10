78b79ceed08a9ab65e5d768424517b01edbd9748
Ashir Ijaz <aashirijazsatti@gmail.com>
2024-12-10 18:52:37 +0500

    initializing

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'loginpage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Removes the debug banner
+      title: 'Safe Space App',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const LoginPage(), // Set the initial screen
+    );
+  }
+}
